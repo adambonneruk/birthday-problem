e3982c367cfcee5d5de9c4e055346892ad6e515b
adambonneruk <adam@bonner.uk>
2022-07-11 23:09:11 +0100

    fixed 2 decimal places code

--- solve.py
+++ solve.py
@@ -8,4 +8,4 @@ for i in range(days,days-people,-1):
 	running_sum = running_sum * (i/days)
 
 pa = 1 - running_sum
-print(str(float(f'{pa:.4f}')*100) + "%")
+print(str(float(f'{pa*100:.2f}')) + "%")
